6b9580db79b0911b6652addb9d05cf5c2960a6da
Mahipatsinh Jadav <mj@mojaave.com>
2018-09-27 15:37:33 +0530

    update gtm code

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,31 +5,27 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-import React from 'react';
-import { oneLine } from 'common-tags';
+const React = require('react');
+const { oneLine } = require('common-tags');
 // Google GST (gtag.js):
 // https://github.com/gatsbyjs/gatsby/issues/8341
-const onRenderBody = ({ setHeadComponents }) => {
+exports.onRenderBody = ({ setHeadComponents }) => {
   const GTM_ID = 'AW-817882221';
-  if (process.env.NODE_ENV === 'production') {
-    setHeadComponents([
-      <script
-        key="google-gtm-js"
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
-      />,
-      <script
-        key="google-gst-init"
-        dangerouslySetInnerHTML={{
-          __html: oneLine`
+  setHeadComponents([
+    <script
+      key="google-gtm-js"
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+    />,
+    <script
+      key="google-gtm-init"
+      dangerouslySetInnerHTML={{
+        __html: oneLine`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${GTM_ID}');`,
-        }}
-      />,
-    ]);
-  }
+      }}
+    />,
+  ]);
 };
-
-export default onRenderBody;
